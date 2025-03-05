c0434bc59ebe445cf8c8f99df8c59def69610ebf
Adiuleson Arlen <adiulesonarlen.ads@gmail.com>
2025-03-05 19:32:24 -0300

    Classes em Java

---
+++ 1_Projetos/m1s03backendjava/src/ClasseCarro.java
@@ -0,0 +1,11 @@
+public class ClasseCarro {
+    String cor;
+    int qtdPortas;
+    int tamanhoRodas;
+    String fabricante;
+    int anoDeFabricacao;
+
+    void acelerar() {}
+    void buzinar() {}
+    void frear() {}
+}
